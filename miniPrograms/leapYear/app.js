bd78582c7172a18d5bb44e620f08b2e671915645
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-08 12:20:29 +0500

    Added leapYear

---
+++ miniPrograms/leapYear/app.js
@@ -0,0 +1,5 @@
+"use strict";
+let year = 2024;
+let leap = 4; //leap year comes after 4 years
+let leapYear = (year % leap);
+console.log(leapYear); //if answer is 0 then this year is leap 
---
+++ miniPrograms/leapYear/app.ts
@@ -0,0 +1,4 @@
+let year = 2024;
+let leap = 4; //leap year comes after 4 years
+let leapYear = (year%leap);
+console.log(leapYear); //if answer is 0 then this year is leap 
