63f52766a682036b3ee7e079f9929ff94a5bd7c0
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-10 07:58:27 +0500

    Revert "Revert "Added nameCases""
    This reverts commit c6cbe44f4364722960f88f6d07c9cdb24208d6ff.

---
+++ nameCases/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//Assignment No:03
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "Adeena Ramzan"; //storing name in variable
+//Lowercase:
+console.log("In lowerCase: " + personName.toLowerCase());
+//Uppercase:
+console.log("In upperCase: " + personName.toUpperCase());
+//Titlecase:
+console.log("In titleCase: " + personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
