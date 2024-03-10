86f4e075a8df87fc978016299708636ade66c6d9
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-10 07:55:58 +0500

    Added famousQuote

---
+++ famousQuote/app.ts
@@ -0,0 +1,4 @@
+//Assignment No: 04
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks :Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+console.log('William Shakespeare once said,"No legacy is so rich as honesty."');
