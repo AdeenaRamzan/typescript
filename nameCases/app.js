8e120a4c5ea9a8d9a389a34a16ae1d63ef3ff40d
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-10 08:14:25 +0500

    Added nameCases

---
+++ nameCases/app.js
@@ -0,0 +1,10 @@
+"use strict";
+//Assignment No: 03 
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let personName = "Adeena Ramzan"; //Storing person name in a variable
+//Lowercase
+console.log("Lowercase: " + personName.toLowerCase());
+//Uppercase
+console.log("Uppercase: " + personName.toUpperCase());
+//Titlecase
+console.log("Titlecase: " + personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
