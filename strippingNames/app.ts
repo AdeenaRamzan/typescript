9469493bceb8c8e7e0f8c0460591827d672d93cf
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-10 08:44:00 +0500

    Added strippingNames

---
+++ strippingNames/app.ts
@@ -0,0 +1,11 @@
+//Assignment No: 06
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+
+let personName= "\t\n Adeena Ramzan \t\n";//Storing name with whitespaces
+
+//With whitespaces
+console.log(personName);
+
+//Without whitespaces 
+console.log(personName.trim());
+
