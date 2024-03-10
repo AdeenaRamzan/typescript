4d80c0aa0188f37154d052b8800f373ef302513a
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-11 04:12:19 +0500

    Added numberEight

---
+++ numberEight/README.md
@@ -0,0 +1,4 @@
+#typescript
+
+This is my Assignment No: 07
+Q: Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
---
+++ numberEight/app.ts
@@ -0,0 +1,6 @@
+//Assignment No:07
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+console.log(7+1);//Addition
+console.log(9-1);//Subtraction
+console.log(4*2);//Multiplication
+console.log(16/2);//Division
