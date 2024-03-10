05220f12b9bccbc80f53334b3e66bd7f9774a57a
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-11 04:27:58 +0500

    Added numberEight(2)

---
+++ numberEight(2)/README.md
@@ -0,0 +1,7 @@
+#typescript
+
+This is my Assignment No: 08
+
+Q: You should create four lines that look like this:
+console.log(5 + 3)
+Your output should simply be four lines with the number 8 appearing once on each line.
---
+++ numberEight(2)/app.js
@@ -0,0 +1,8 @@
+"use strict";
+// You should create four lines that look like this:
+// console.log(5 + 3)
+// Your output should simply be four lines with the number 8 appearing once on each line.
+console.log(7 + 1);
+console.log(9 - 1);
+console.log(4 * 2);
+console.log(16 / 2);
