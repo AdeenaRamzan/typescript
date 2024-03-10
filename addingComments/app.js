409e5cb852696c3ca34105fc0031b9269ef8e3b8
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-11 04:53:35 +0500

    Added addingComments

---
+++ addingComments/app.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment No:10
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//Name: Adeena Ramzan 
+//Date: 10/03/2024
+//Making variable of my name and printing it
+let myName = "Adeena Ramzan";
+console.log("My Name is " + myName);
