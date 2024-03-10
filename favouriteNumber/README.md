184369027df14e43b2ac7cdb8a509af2251f06f0
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-11 04:39:56 +0500

    Added favouriteNumber

---
+++ favouriteNumber/README.md
@@ -0,0 +1,6 @@
+#typescript
+
+This is my Assignment No:09
+
+Favorite Number: 
+Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
---
+++ favouriteNumber/app.ts
@@ -0,0 +1,7 @@
+// Assignment No:09
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let favouriteNumber = 10; //Storing favourite number
+let message = "My Favourite Number is : "+favouriteNumber; 
+console.log(message);//Printing Message
+
+
