a0ba09db856a92303b2251d9ee5e91a94ca8a076
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-14 09:13:59 +0500

    Update app.ts

--- nameCases/app.js
+++ nameCases/app.js
@@ -1,10 +1,9 @@
-"use strict";
 //Assignment No: 03 
 //Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-let personName = "Adeena Ramzan"; //Storing person name in a variable
+var personName = "Adeena Ramzan"; //Storing person name in a variable
 //Lowercase
-console.log("Lowercase: " + personName.toLowerCase());
+console.log("Lowercase: ".concat(personName.toLowerCase()));
 //Uppercase
-console.log("Uppercase: " + personName.toUpperCase());
+console.log("Uppercase: ".concat(personName.toUpperCase()));
 //Titlecase
-console.log("Titlecase: " + personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+console.log("Titlecase: ".concat(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()));
--- nameCases/app.ts
+++ nameCases/app.ts
@@ -3,9 +3,9 @@
 
 let personName="Adeena Ramzan";//Storing person name in a variable
 //Lowercase
-console.log("Lowercase: "+ personName.toLowerCase());
+console.log(`Lowercase: ${ personName.toLowerCase()}`);
 //Uppercase
-console.log("Uppercase: "+personName.toUpperCase());
+console.log(`Uppercase: ${personName.toUpperCase()}`);
 //Titlecase
-console.log("Titlecase: "+personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+console.log(`Titlecase: ${personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()}`);
 
