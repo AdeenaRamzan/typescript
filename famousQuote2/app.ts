af00db861a416d79707290b092bc90604d7de85e
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-14 09:10:41 +0500

    Update app.ts

--- famousQuote2/app.ts
+++ famousQuote2/app.ts
@@ -2,5 +2,5 @@
 //Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 
 let famous_person:string="W. E. Hickson";
-let message:string=famous_person+" once said: If at first you don't succeed, try, try again.";
+let message:string=`${famous_person}" once said: If at first you don't succeed, try, try again.`;
 console.log(message);
