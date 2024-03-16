a6a61f6d7848b59ac8c3047e9cd4a95b01121310
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-16 09:47:24 +0500

    modified

--- PROJECTS/project_calculator/main.ts
+++ PROJECTS/project_calculator/main.ts
@@ -1,38 +1,37 @@
 import inquirer from "inquirer";
 
-const answer= await inquirer.prompt([
-    {
-        message: "Enter First Number",
-        type:"number",
-        name:"firstNumber",
-    
-    },
-    {
-        message: "Enter Second Number",
-        type:"number",
-        name:"secondNumber",
-    
-    },
-    {
-        message: "Select any Operator to perform action",
-        type:"list",
-        name:"operator",
-        choices:["Addition","Subtraction","Multiplication","Division"],
-    
-    },
-
+const answer = await inquirer.prompt([
+  {
+    message: "Enter First Number",
+    type: "number",
+    name: "firstNumber",
+  },
+  {
+    message: "Enter Second Number",
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: "Select any one Operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
 ]);
-if(answer.operator==="Addition"){
-    let add=answer.firstNumber+answer.secondNumber
-    console.log("Your answer is: "+add);
+//Conditional Statements
+if (answer.operator === "Addition") {
+  let add = answer.firstNumber + answer.secondNumber;
+  console.log("The answer is: " + add);
 }
-if(answer.operator==="Subtraction"){
-    let sub=answer.firstNumber-answer.secondNumber
-    console.log("Your answer is: "+sub);
-}if(answer.operator==="Multiplication"){
-    let mul=answer.firstNumber*answer.secondNumber
-    console.log("Your answer is: "+mul);
-}if(answer.operator==="Division"){
-    let div=answer.firstNumber/answer.secondNumber
-    console.log("Your answer is: "+div);
+if (answer.operator === "Subtraction") {
+  let sub = answer.firstNumber - answer.secondNumber;
+  console.log("The answer is: " + sub);
+}
+if (answer.operator === "Multiplication") {
+  let multiply = answer.firstNumber * answer.secondNumber;
+  console.log("The answer is: " + multiply);
+}
+if (answer.operator === "Division") {
+  let divide = answer.firstNumber / answer.secondNumber;
+  console.log("The answer is: " + divide);
 }
