ec3fb2a5dcdfda86e19c7edea20406d643406b99
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-29 02:07:04 +0500

    Added names

---
+++ names/README.md
@@ -0,0 +1,3 @@
+#typescript This is my Assignment No: 11
+
+Q11.Names:Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
---
+++ names/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+Object.defineProperty(exports, "__esModule", { value: true });
+let friendsName = ["Adeena", "Hadia", "Rameen", "Hafsa"];
+for (let i = 0; i < friendsName.length; i++) {
+    console.log(friendsName[i]);
+}
