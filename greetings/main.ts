9f7f61738d257f44953b0924d833264e8dc61c6a
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-29 02:13:09 +0500

    Added greetings

---
+++ greetings/main.ts
@@ -0,0 +1,6 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let friendsName=["Adeena","Rameen","Hadia","Hafsa"];
+for(let i=0;i<friendsName.length;i++){
+    console.log("Hello "+friendsName[i]+" ,How are you? What are you doing");
+}
