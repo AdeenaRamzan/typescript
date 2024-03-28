a8031cd1e56c3c351ccad1764680db099dc46411
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-03-29 02:56:02 +0500

    Added changingGuestList

---
+++ changingGuestList/README.md
@@ -0,0 +1,11 @@
+#typescript This is my Assignment No: 15
+
+Q15.Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+
+• Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+
+• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+
+• Print a second set of invitation messages, one for each person who is still in your list.
---
+++ changingGuestList/main.js
@@ -0,0 +1,14 @@
+"use strict";
+// Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+Object.defineProperty(exports, "__esModule", { value: true });
+// • Start with your program from Exercise 14. Add a print statement at the end of your program stating the name of the guest who can’t make it.
+// • Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+// • Print a second set of invitation messages, one for each person who is still in your list.
+let guestList = ["Adeena", "Hadia", "Rameen"];
+let canNotAttend = "Hadia";
+let newGuest = "Ayesha";
+let index = guestList.indexOf(canNotAttend);
+guestList[index] = newGuest;
+for (let i = 0; i < guestList.length; i++) {
+    console.log(`Dear ${guestList[i]}, I would like to invite you to dinner.`);
+}
