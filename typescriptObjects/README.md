74e239cb3ab2f42c2c2b817967df6bdabe74bbba
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-04-01 08:18:15 +0500

    Added typescriptObject

---
+++ typescriptObjects/README.md
@@ -0,0 +1,4 @@
+#typescript This is my Assignment No: 21
+
+
+Q21:They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
---
+++ typescriptObjects/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let Car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020
+};
+console.log(`Car make ${Car.make} of model ${Car.model} launched in year ${Car.year}`);
