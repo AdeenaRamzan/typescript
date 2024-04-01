868e8152c040e4cde09c637ece0cf536371c73e5
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-04-01 07:57:45 +0500

    Added dinnerGuests

---
+++ dinnerGuests/README.md
@@ -0,0 +1,4 @@
+#typescript This is my Assignment No: 19
+
+Q19.Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
---
+++ dinnerGuests/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+//From Assignment No:14 GuestList
+let guestList = ["Adeena", "Hadia", "Rameen"];
+// for(let i=0;i<guestList.length;i++){  
+//     console.log(`Dear ${guestList[i]}, I would like to invite you to dinner.`)
+// }
+let lengthOfGuestList = guestList.length;
+console.log(`Number of Guests Invited:${lengthOfGuestList}`);
