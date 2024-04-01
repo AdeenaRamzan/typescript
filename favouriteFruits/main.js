1639c12218529870bfbb3c23cb95c495f7c6e38c
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-04-02 01:09:18 +0500

    Added favouriteFruits & helloAdmin

---
+++ favouriteFruits/main.js
@@ -0,0 +1,18 @@
+"use strict";
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+Object.defineProperty(exports, "__esModule", { value: true });
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+let favorite_fruits = ["mango", "banana", "orange"];
+if (favorite_fruits.includes("mango")) {
+    console.log("You really like mango!");
+}
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like banana!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like orange!");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apple!");
+}
---
+++ favouriteFruits/main.ts
@@ -0,0 +1,19 @@
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+let favorite_fruits: string[] = ["mango", "banana", "orange"];
+
+if (favorite_fruits.includes("mango")) {
+  console.log("You really like mango!");
+}
+if (favorite_fruits.includes("banana")) {
+    console.log("You really like banana!");
+}
+if (favorite_fruits.includes("orange")) {
+    console.log("You really like orange!");
+}
+if (favorite_fruits.includes("apple")) {
+    console.log("You really like apple!");
+}
