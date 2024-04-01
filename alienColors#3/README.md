4454d5cf55b46285e0248a32e7ab41b525aaea54
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-04-01 17:40:56 +0500

    Added alienColors#3

---
+++ alienColors#3/README.md
@@ -0,0 +1,9 @@
+#typescript This is my Assignment No: 27
+
+
+Q27:Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+• If the alien is green, print a message that the player earned 5 points.
+
+• If the alien is yellow, print a message that the player earned 10 points.
+
+• If the alien is red, print a message that the player earned 15 points.
---
+++ alienColors#3/main.js
@@ -0,0 +1,39 @@
+"use strict";
+// Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+// • If the alien is green, print a message that the player earned 5 points.
+Object.defineProperty(exports, "__esModule", { value: true });
+// • If the alien is yellow, print a message that the player earned 10 points.
+// • If the alien is red, print a message that the player earned 15 points.
+//green
+let alienColor = "green";
+if (alienColor == "green") {
+    console.log("The alien is green, the player has earned 5 points.");
+}
+else if (alienColor == "yellow") {
+    console.log("The alien is yellow, the player has earned 10 points.");
+}
+else if (alienColor == "red") {
+    console.log("The alien is red, the player has earned 15 points.");
+}
+//yellow
+let alienColor2 = "yellow";
+if (alienColor2 == "green") {
+    console.log("The alien is green, the player has earned 5 points.");
+}
+else if (alienColor2 == "yellow") {
+    console.log("The alien is yellow, the player has earned 10 points.");
+}
+else if (alienColor2 == "red") {
+    console.log("The alien is red, the player has earned 15 points.");
+}
+//red
+let alienColor3 = "red";
+if (alienColor3 == "green") {
+    console.log("The alien is green, the player has earned 5 points.");
+}
+else if (alienColor3 == "yellow") {
+    console.log("The alien is yellow, the player has earned 10 points.");
+}
+else if (alienColor3 == "red") {
+    console.log("The alien is red, the player has earned 15 points.");
+}
