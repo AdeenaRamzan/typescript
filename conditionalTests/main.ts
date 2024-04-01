7471854b234e4aaf5b07a794efd4aa5e5d883ee5
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-04-01 08:37:12 +0500

    Added conditionalTests

---
+++ conditionalTests/main.ts
@@ -0,0 +1,53 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// let car = 'subaru';
+
+// console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let favProgrammingLanguage = 'JavaScript';
+
+//True
+console.log("Is favProgrammingLanguage == 'JavaScript'?I predict True");
+console.log(favProgrammingLanguage == 'JavaScript');
+
+//False
+console.log("Is favProgrammingLanguage == 'Python'?I predict False");
+console.log(favProgrammingLanguage == 'Python');
+
+//True
+console.log("Is favProgrammingLanguage == 'JavaScript'?I predict True");
+console.log(favProgrammingLanguage == 'JavaScript');
+
+//False
+console.log("Is favProgrammingLanguage == 'Java'?I predict False");
+console.log(favProgrammingLanguage == 'Java');
+
+//True
+console.log("Is favProgrammingLanguage == 'JavaScript'?I predict True");
+console.log(favProgrammingLanguage == 'JavaScript');
+
+//False
+console.log("Is favProgrammingLanguage == 'C#'?I predict False");
+console.log(favProgrammingLanguage == 'C#');
+
+//True
+console.log("Is favProgrammingLanguage == 'JavaScript'?I predict True");
+console.log(favProgrammingLanguage == 'JavaScript');
+
+//False
+console.log("Is favProgrammingLanguage == 'C++'?I predict False");
+console.log(favProgrammingLanguage == 'C++');
+
+//True
+console.log("Is favProgrammingLanguage == 'JavaScript'?I predict True");
+console.log(favProgrammingLanguage == 'JavaScript');
+
+//False
+console.log("Is favProgrammingLanguage == 'C'?I predict False");
+console.log(favProgrammingLanguage == 'C');
