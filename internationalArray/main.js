40ad53c935d1a0d69821691160d2cacc10fd8e0d
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-04-01 08:24:15 +0500

    Added internationalArray

---
+++ internationalArray/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+Object.defineProperty(exports, "__esModule", { value: true });
+let errorArray = ["a", "b", "c", "d"];
+//Producing index error...
+// console.log(errorArray[5]);
+//Corrected Error
+console.log(errorArray[1]);
