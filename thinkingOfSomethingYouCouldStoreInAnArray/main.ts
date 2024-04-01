9a5fe43e1e9367bf6ef855fdb1948e1a0baff88b
Adeena Ramzan <aradeenaramzan@gmail.com>
2024-04-01 08:07:41 +0500

    Added Assignment 20

---
+++ thinkingOfSomethingYouCouldStoreInAnArray/main.ts
@@ -0,0 +1,6 @@
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let countries :string[]= ["Pakistan","England","New Zealand","Dubai","Turkey"];
+for(let i=0;i<countries.length;i++){
+    console.log(countries[i]);
+};
